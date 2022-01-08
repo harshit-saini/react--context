96f2be70b743a21bff8ff81719e119137f35b110
harshit-saini <harshitr.x.t.10@gmail.com>
2022-01-08 21:54:53 +0530

    use the context with contextType variable in the class

---
+++ src/components/Div.js
@@ -0,0 +1,42 @@
+/* 
+this is a class component and we will learn here how to use 
+context in a class component 
+
+1. 
+if we have only one context to use we can use the special variable ```contextType```
+  to use the context.
+
+2. 
+if we have to use more than one component then we have to use the ```context.consumer```
+
+
+*/
+
+import React, { Component } from 'react'
+import { ThemeContext } from '../context/ThemeContext';
+
+const divStyle = {
+  border: "2px solid black"
+}
+
+class Div extends Component {
+
+  static contextType = ThemeContext;
+
+
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+
+    const theme = isLightTheme ? light : dark;
+
+    return (
+      <div style={{ backgroundColor: theme.background, color: theme.text, ...divStyle }}>
+        <h1>Harshit saini</h1>
+      </div>
+
+    )
+  }
+
+}
+
+export default Div;
